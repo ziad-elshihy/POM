e377143adb3e402e2ebf4aced2454c0dd9d515ee
Ziad Elshihy <elshihyziad@gmail.com>
2025-10-19 22:52:51 +0300

    Initial commit

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.By;
+
+public class LoginPage {
+    By usernameField = By.id("user-name"); // standard_user
+    By passwordField = By.id("password"); // secret_sauce
+    By loginButton = By.id("login-button");
+    //url: https://www.saucedemo.com/
+}
