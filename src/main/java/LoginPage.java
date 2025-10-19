38b59492b8b6e83121f3689edd5a1ae805bd8be0
Ziad Elshihy <elshihyziad@gmail.com>
2025-10-20 00:25:10 +0300

    Add LoginPage class and LoginTest class and designed two test cases (valid and invalid login) with POM Design pattern

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,8 +1,31 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Objects;
 
 public class LoginPage {
-    By usernameField = By.id("user-name"); // standard_user
-    By passwordField = By.id("password"); // secret_sauce
-    By loginButton = By.id("login-button");
+    // Variables
+    WebDriver driver;
+
+    // Locators
+    private final By usernameField = By.id("user-name"); // standard_user
+    private final By passwordField = By.id("password"); // secret_sauce
+    private final By loginButton = By.id("login-button");
     //url: https://www.saucedemo.com/
+
+    // Constructor
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    // Methods
+    public void login(String username, String password){
+        driver.findElement(usernameField).sendKeys(username);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+
+    public boolean isLoggedIn(String expectedUrl){
+        return Objects.equals(driver.getCurrentUrl(), expectedUrl);
+    }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,41 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class LoginTest {
+    // init driver
+    WebDriver driver;
+
+    // Tests
+    @Test
+    public void validLoginTC() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user","secret_sauce");
+        Assert.assertTrue(loginPage.isLoggedIn("https://www.saucedemo.com/inventory.html"));
+    }
+
+    @Test
+    public void inValidLoginTC() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard","secret_sauce");
+        Assert.assertTrue(loginPage.isLoggedIn("https://www.saucedemo.com/inventory.html"));
+    }
+
+    // Configuration and setup methods would go here
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
